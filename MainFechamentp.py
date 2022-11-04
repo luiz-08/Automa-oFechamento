b395349d0c024dfb3508d9145f7c060cae874f64
luiz-08 <luiz.gustavo2928@gmail.com>
2022-11-03 22:48:13 -0300

    Alteração codigo fonte no Main e adição da notificacao junto com a imagem

--- MainFechamentp.py
+++ MainFechamentp.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from notifypy import Notify
 
 import time
 
@@ -19,6 +20,7 @@ driver.implicitly_wait(25)
 #acessar elementos dentro do iframe driver.switch_to("iframe" )
 #PyWinAuto para controle do APRJ
 
+
 logar = driver.find_element(by=By.XPATH, value= "//a[@id='loginBtn']")
 logar.click()
 
@@ -31,8 +33,30 @@ Credenciais.send_keys(PassWord)
 Submit = driver.find_element(by=By.XPATH, value= "//a[@id='loginBtn']")
 Submit.click()
 
-Token = driver.find_element(by=By.ID, value="passwd")
-Token.send_keys(twofa)
+
+time.sleep(2)
+
+notification = Notify()
+notification.title = "BOT FECHAMENTO"
+notification.message = "Por favor coloque seu código do segundo fator de autenticação (Token)"
+notification.icon = "MicrosoftAuthenticator1.png"
+
+notification.send()
+
+#Analisar o detectar receivcer
+
+Allapps = driver.find_element(by=By.XPATH, value="//a[@id='allAppsBtn']")
+Allapps.click()
+
+App = driver.find_element(by=By.XPATH, value="//img[@alt='EasyIRPJ']")
+App.click()
+
+
+
+
+
+
+
 
 
 
